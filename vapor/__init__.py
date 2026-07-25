edc18b1194880cc309bae14694ac6bf93b925e78
Mike Lindsey <48146860+cyberneticlawngnome@users.noreply.github.com>
2026-07-25 11:44:25 -0700

    refactor: create vapor package structure

---
+++ vapor/__init__.py
@@ -0,0 +1,10 @@
+"""
+Vapor ROM Manager - A multi-platform ROM and emulation stack manager.
+"""
+
+__version__ = "0.2.0"
+__author__ = "cyberneticlawngnome"
+
+from vapor.core.logger import setup_logger
+
+__all__ = ["setup_logger"]
